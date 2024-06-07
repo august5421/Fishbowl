191856fbf6ae8e542138cf390926897636a42d02
JackTh3 <166152697+JackTh3@users.noreply.github.com>
2024-06-07 16:11:25 -0400

    autoscroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Box, IconButton, Typography, Drawer, Button, Fade } from '@mui/material';
-import { setScrollPosition, setSettingDrawer, setScreenState, resetPlayerIndex, setTeams, setNumberOfEntries, setPlayerCount, setTimeLimit } from './actions/actions';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { Box, IconButton, Typography, Drawer } from '@mui/material';
+import LogoFont from './components/LogoFont';
+import { Button, Fade } from '@mui/material';
+import './app.css';
+import { setScrollPosition, setSettingDrawer, setScreenState, resetPlayerIndex, setTeams } from './actions/actions';
+import PlayerForm from './components/PlayerForm.jsx';
 import SettingsIcon from '@mui/icons-material/Settings';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import StopIcon from '@mui/icons-material/Stop';
-import LogoFont from './components/LogoFont';
-import PlayerForm from './components/PlayerForm';
-import SettingScreen from './screens/SettingScreen';
-import GameScreen from './screens/GameScreen';
+import SettingScreen from './screens/SettingScreen.jsx';
 import sound from './assets/mp3/bubbles.mp3';
-import './app.css';
+import GameScreen from './screens/GameScreen.jsx';
+import StopIcon from '@mui/icons-material/Stop';
 
-const App = () => {
+function App() {
   const dispatch = useDispatch();
   const theme = useSelector((state) => state.theme);
   const screenState = useSelector((state) => state.screenState);
@@ -21,27 +22,14 @@ const App = () => {
   const settingDrawer = useSelector((state) => state.settingDrawer);
   const mobile = useSelector((state) => state.mobile);
 
-  // Utility function to find the nearest multiple of 100
-  const nearestMultipleOf100 = (value) => {
-    return Math.round(value / 100) * 100;
-  };
-
-  // useEffect to listen for changes in scrollPosition.onBoardingBlock
-  useEffect(() => {
-    if (scrollPosition.onBoardingBlock % 100 !== 0) {
-      const correctedPosition = nearestMultipleOf100(scrollPosition.onBoardingBlock);
-      dispatch(setScrollPosition('onBoardingBlock', correctedPosition));
-    }
-  }, [scrollPosition.onBoardingBlock, dispatch]);
-
   const handleScroll = () => {
     if (scrollPosition.main > -1900) {
       dispatch(setScrollPosition('main', scrollPosition.main - 100));
     }
     dispatch(setScrollPosition('onBoardingBlock', scrollPosition.main - 100));
     const audio = new Audio(sound);
-    audio.play().catch((error) => {
-      console.error('Failed to play audio:', error);
+    audio.play().catch(error => {
+        console.error("Failed to play audio:", error);
     });
   };
 
@@ -57,64 +45,43 @@ const App = () => {
     dispatch(setScreenState('start'));
     dispatch(resetPlayerIndex([{ playerName: '', entries: ['', '', ''] }]));
     dispatch(setNumberOfEntries(3));
-    dispatch(setTimeLimit(60));
+    dispatch(setTimeLimit(60)); 
     dispatch(setPlayerCount(1));
-    dispatch(setTeams({ teamOne: [], teamTwo: [] }));
-  };
+    dispatch(setTeams({
+      teamOne: [],
+      teamTwo: []
+  }));
+  }
 
   const handlePlay = () => {
     const players = [...playerIndex];
     const shuffledPlayers = players.sort(() => 0.5 - Math.random());
-    const filteredPlayers = shuffledPlayers.filter((player) => player.playerName !== '');
+    const filteredPlayers = shuffledPlayers.filter(player => player.playerName !== '');
     dispatch(resetPlayerIndex(filteredPlayers));
     const midIndex = Math.ceil(filteredPlayers.length / 2);
-    const teamOne = filteredPlayers.slice(0, midIndex).map((player) => player.playerName);
-    const teamTwo = filteredPlayers.slice(midIndex).map((player) => player.playerName);
+    const teamOne = filteredPlayers.slice(0, midIndex).map(player => player.playerName);
+    const teamTwo = filteredPlayers.slice(midIndex).map(player => player.playerName);
 
     dispatch(setTeams({ teamOne, teamTwo }));
 
     dispatch(setScrollPosition('main', 0));
     dispatch(setScrollPosition('onBoardingBlock', 0));
     const audio = new Audio(sound);
-    audio.play().catch((error) => {
-      console.error('Failed to play audio:', error);
+    audio.play().catch(error => {
+        console.error("Failed to play audio:", error);
     });
     setTimeout(() => {
-      dispatch(setScreenState('game'));
+        dispatch(setScreenState('game'));
     }, 250);
-  };
+};
 
   return (
     <>
-      <Box
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          transition: 'top 1s ease-in-out',
-          height: 'calc(100vh * 20)',
-          position: 'absolute',
-          top: `${scrollPosition.main}vh`,
-          left: 0,
-          right: 0,
-          backgroundImage: `linear-gradient(360deg, ${theme.black} 0%, ${theme.blueOne} 20%, ${theme.blueTwo} 58%, ${theme.blueThree} 100%)`,
-        }}
-      ></Box>
-      <Box
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          position: 'absolute',
-          transition: 'top 1s ease-in-out',
-          height: 'calc(100vh * 19)',
-          top: `${scrollPosition.onBoardingBlock}vh`,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          zIndex: 10,
-        }}
-      >
+      <Box style={{display: 'flex', justifyContent: 'center', alignItems: 'center', transition: 'top 1s ease-in-out', height: 'calc(100vh * 20)', position: 'absolute', top: `${scrollPosition.main}vh`, left: 0, right: 0, backgroundImage: 
+        `linear-gradient(360deg, ${theme.black} 0%, ${theme.blueOne} 20%, ${theme.blueTwo} 58%, ${theme.blueThree} 100%)`
+      }}>
+      </Box>
+      <Box style={{display: 'flex', flexDirection: "column", alignItems: 'center', position: 'absolute', transition: 'top 1s ease-in-out', height: 'calc(100vh * 19)', top: `${scrollPosition.onBoardingBlock}vh`, left: 0, right: 0, bottom: 0, zIndex: 10}}>
         <Box
           sx={{
             position: 'fixed',
@@ -123,11 +90,11 @@ const App = () => {
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
-            zIndex: 11,
+            zIndex: 11
           }}
         >
-          <IconButton style={{ color: theme.white }} onClick={handleSettings}>
-            <SettingsIcon style={{ color: theme.white }} />
+          <IconButton style={{color: theme.white}} onClick={handleSettings}>
+            <SettingsIcon style={{color: theme.white}} />
           </IconButton>
         </Box>
         <Fade in={playerIndex.length >= 4}>
@@ -139,66 +106,46 @@ const App = () => {
               display: 'flex',
               flexDirection: 'column',
               alignItems: 'center',
-              zIndex: 11,
+              zIndex: 11
             }}
           >
             {screenState === 'start' ? (
-              <IconButton style={{ color: theme.white }} onClick={handlePlay}>
-                <PlayArrowIcon style={{ color: theme.white }} />
+              <IconButton style={{color: theme.white}} onClick={handlePlay}>
+                <PlayArrowIcon style={{color: theme.white}} />
               </IconButton>
             ) : (
-              <IconButton style={{ color: theme.white }} onClick={handleStop}>
-                <StopIcon style={{ color: theme.white }} />
+              <IconButton style={{color: theme.white}} onClick={handleStop}>
+                <StopIcon style={{color: theme.white}} />
               </IconButton>
             )}
           </Box>
         </Fade>
         {screenState === 'game' && (
           <Fade in={screenState === 'game'}>
-            <Box style={{ display: 'flex', flex: 1, width: '100vw' }}>
+            <Box style={{display: 'flex', flex: 1, width: '100vw'}}>
               <GameScreen />
             </Box>
           </Fade>
         )}
         <Fade in={screenState === 'start'}>
           <Box>
-            <Box
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100vh',
-              }}
-            >
+            <Box style={{display: 'flex', flexDirection: "column", justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
               <LogoFont text="Fishbowl" color={theme.white} fontSize="60px" fontWeight={600} />
               <Button
                 onClick={handleScroll}
                 style={{
                   color: theme.white,
                   borderColor: theme.white,
-                }}
-                variant="outlined"
-              >
-                Start!
-              </Button>
+                }} variant="outlined">Start!</Button>
             </Box>
             {playerIndex.map((player, index) => (
-              <Box
-                key={index}
-                style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  height: '100vh',
-                }}
-              >
-                <PlayerForm index={index} />
-              </Box>
+                <Box key={index} style={{display: 'flex', flexDirection: "column", justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+                    <PlayerForm index={index} />
+                </Box>
             ))}
           </Box>
         </Fade>
+        
       </Box>
       <Drawer
         open={settingDrawer}
@@ -211,10 +158,11 @@ const App = () => {
           },
         }}
       >
+        
         <SettingScreen />
       </Drawer>
     </>
   );
-};
+}
 
 export default App;
